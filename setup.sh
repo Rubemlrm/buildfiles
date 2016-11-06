8c0171aeb1a4d4e7f30d3371e09a804aa6a64b2c
rubem mota <rubemlrm@dojo.lan>
2016-11-06 17:08:12 +0000

    fixed recursive symlinks from setup.sh, other minor stuff

--- setup.sh
+++ setup.sh
@@ -8,18 +8,24 @@ mkdir -p ~/bin
 echo "cloning needed packages"
 
 echo "Cloning urxvt perl extensions"
-mkdir -p ~/.urxvt/ext
+if [ ! -e ~/.urxvt/ext ]; then
+    mkdir -p ~/.urxvt/ext
+fi
 git clone git@github.com:michaeltraxler/urxvt-perls.git ~/.urxvt/ext
 
 echo "cloning oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -f -s $CWD/zsh/zshrc ~/.zshrc
-ln -f -s $CWD/zsh/configs ~/.config/zsh
+
+if [ ! -e ~/.config/zsh/ ]; then
+    ln -s $CWD/zsh/configs ~/.config/zsh
+    touch ~/.config/zsh/development
+fi
 
 echo "setup rvm"
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s -- --path ~/bin/rvm 
-cat >> ~/.configs/zsh/development << EOM
+cat >> ~/.config/zsh/development << EOM
 #rvm
 export rvm_path="$HOME"
 if [ -e "$HOME/bin/rvm/scripts/rvm" ]; then
@@ -30,29 +36,35 @@ EOM
 
 echo "setup perl"
 \curl -L https://install.perlbrew.pl | bash
-cat >> ~/.configs/zsh/development << EOM
+cat >> ~/.config/zsh/development << EOM
 #perlbrew
 if [ -e "$HOME/bin/perl/etc/bashrc" ]; then
-  source ~/bin/perl/etc/bashrc" >> ~/.zshrc
+  source $HOME/bin/perl/etc/bashrc
 fi
 
 EOM
 
 echo "setup nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-cat >> ~/.configs/zsh/development << EOM
-#nvm
-export NVM_DIR="$HOME/bin/nvm"
-if [ -e "$NVM_DIR/nvm.sh" ]; then
-    source $NVM_DIR/nvm.sh
-fi
-EOM
 
 
 echo "creating symlinks"
-ln -f -s $CWD/vim/vimrc ~/.vimrc
-ln -f -s $CWD/vim/vim ~/.vim
-ln -f -s $CWD/Xstuff/Xdefaults ~/.Xdefaults
-ln -f -s $CWD/tmux/tmux.conf ~/.tmux.conf
-ln -f -s $CWD/tmux/configs ~/.config/tmux
+if [ ! -e ~/.vimrc ]; then
+    ln -s $CWD/vim/vimrc ~/.vimrc
+fi
 
+if [ ! -e ~/.vim ]; then
+    ln -s $CWD/vim/vim ~/.vim
+fi
+
+if [ ! -e ~/.Xdefaults ]; then
+    ln -s $CWD/Xstuff/Xdefaults ~/.Xdefaults
+fi
+
+if [ ! -e ~/.tmux.conf ]; then
+    ln -s $CWD/tmux/tmux.conf ~/.tmux.conf
+fi
+
+if [ ! -e ~/.config/tmux ]; then
+    ln -f t current window-s $CWD/tmux/configs ~/.config/tmux
+fi
