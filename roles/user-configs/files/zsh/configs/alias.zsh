ad96351a5f19d4c1de9c09bc2980b27b1c9fdf5d
Rubem <rubemmota89@gmail.com>
2019-04-30 22:18:48 +0100

    Updated zsh configs
    Updated zsh configs with new alias and functions.
    Removed some uneeded plugins from zsh.
    Removed ruby  tasks from development-tools.

--- roles/user-configs/files/zsh/configs/alias.zsh
+++ roles/user-configs/files/zsh/configs/alias.zsh
@@ -1,5 +1,26 @@
+#directories
 alias works="cd ~/Works"
-alias h='history'
-alias ports='netstat -tulanp'
-alias irssi='TERM=screen-256color irssi'
 alias backupDconf="dconf dump /org/cinnamon/ >> /home/rubemlrm/Works/dotfiles/roles/user-configs/files/dconf/dconf.bak && cd /home/rubemlrm/Works/dotfiles && git add ./roles/user-configs/files/dconf/ && git commit -m 'updated dconf settings'"
+#networking
+alias ports='netstat -tulanp'
+alias kodiSSH="ssh osmc@192.168.1.72"
+alias piHoleSSH="ssh -p1313 rubemlrm@192.168.1.71"
+alias hosts="sudo nano /etc/hosts"
+alias sysinfo="sudo rpm -qi basesystem"
+alias speedtest="wget -O /dev/null https://speed.hetzner.de/100MB.bin"
+#system utils
+alias h='history'
+alias g="git"
+alias zreload="source ~/.zshrc"
+alias sf="screenfetch"
+alias psa="ps aux"
+alias psg="ps aux | grep "
+# vimrc editing
+alias ve='vim ~/.vimrc'
+# zsh profile editing
+alias ze='vim ~/.zshrc'
+# open currenct directory on nautilus
+alias op="nautilus . > /dev/null 2>&1"
+#apps
+alias irssi='TERM=screen-256color irssi'
+
