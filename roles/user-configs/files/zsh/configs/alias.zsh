0a1ced6230bb31a4dcb34134598e8ded396674ec
Rubem <rubemmota89@gmail.com>
2019-04-30 17:57:31 +0100

    Removed inline gnome settings, added alias to backup dconf settings for cinnamon

--- roles/user-configs/files/zsh/configs/alias.zsh
+++ roles/user-configs/files/zsh/configs/alias.zsh
@@ -1,4 +1,5 @@
-alias webdev="cd ~/Works/WebDev"
+alias works="cd ~/Works"
 alias h='history'
 alias ports='netstat -tulanp'
 alias irssi='TERM=screen-256color irssi'
+alias backupDconf="dconf dump /org/cinnamon/ >> /home/rubemlrm/Works/dotfiles/roles/user-configs/files/dconf/dconf.bak && cd /home/rubemlrm/Works/dotfiles && git add ./roles/user-configs/files/dconf/ && git commit -m 'updated dconf settings'"
